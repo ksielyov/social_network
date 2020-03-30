d40cca9dde57df32f3687e3166b87e788842c6fa
System Administrator <root@MacBook-Pro-MacBook.local>
2020-03-30 15:52:50 +0500

    CSS module was added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-import Header from './component/Header'
-import Navbar from './component/Navbar'
-import Profile from './component/Profile'
+import Header from './component/Header';
+import Navbar from './component/Navbar';
+import Profile from './component/Profile';
 
 function App()
 {
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import Class from './Navbar.module.css';
 
 const Navbar = () => {
     return(
-        <nav className='nav'>
+        <nav className={ Class.nav }>
             <ul>
                 <li><a><i class="fa fa fa-user" aria-hidden="true"></i> Profile</a></li>
                 <li><a><i class="fa fa-newspaper-o" aria-hidden="true"></i> News</a></li>
