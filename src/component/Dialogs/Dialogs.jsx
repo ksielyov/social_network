a11782808451134aaaf61c994c9f935fad161b3d
ksielyov <mihailkiseliow@yandex.ru>
2020-04-02 16:19:49 +0500

    The messages component was added.

--- src/component/Dialogs/Dialogs.jsx
+++ src/component/Dialogs/Dialogs.jsx
@@ -3,7 +3,91 @@ import Class from './Dialogs.module.css';
 
 const Dialogs = () => {
     return (
-        <div className={Class.content}>Dialogs</div>
+        <div className={Class.dialogsWrapper}>
+            <div className={Class.dialogs}>
+                <div className={Class.message}>
+                    <img src="https://miro.medium.com/max/2400/1*hgVVYMtzC2FjK221f-6wyA.png" alt="" />
+                    <div className={Class.messageInfo}>
+                        <p>Igor Link</p>
+                        <span><span style={{ color: '#777' }}>Elon:</span> is awazing. I am</span>
+                    </div>
+                </div>
+                <div className={Class.message}>
+                    <img src="https://miro.medium.com/max/2400/1*hgVVYMtzC2FjK221f-6wyA.png" alt="" />
+                    <div className={Class.messageInfo}>
+                        <p>Igor Link</p>
+                        <span><span style={{ color: '#777' }}>Elon:</span> is awazing. I am</span>
+                    </div>
+                </div>
+                <div className={Class.message}>
+                    <img src="https://miro.medium.com/max/2400/1*hgVVYMtzC2FjK221f-6wyA.png" alt="" />
+                    <div className={Class.messageInfo}>
+                        <p>Igor Link</p>
+                        <span><span style={{ color: '#777' }}>Elon:</span> is awazing. I am</span>
+                    </div>
+                </div>
+                <div className={Class.message}>
+                    <img src="https://miro.medium.com/max/2400/1*hgVVYMtzC2FjK221f-6wyA.png" alt="" />
+                    <div className={Class.messageInfo}>
+                        <p>Igor Link</p>
+                        <span><span style={{ color: '#777' }}>Elon:</span> is awazing. I am</span>
+                    </div>
+                </div>
+                <div className={Class.message}>
+                    <img src="https://miro.medium.com/max/2400/1*hgVVYMtzC2FjK221f-6wyA.png" alt="" />
+                    <div className={Class.messageInfo}>
+                        <p>Igor Link</p>
+                        <span><span style={{ color: '#777' }}>Elon:</span> is awazing. I am</span>
+                    </div>
+                </div>
+                <div className={Class.message}>
+                    <img src="https://miro.medium.com/max/2400/1*hgVVYMtzC2FjK221f-6wyA.png" alt="" />
+                    <div className={Class.messageInfo}>
+                        <p>Igor Link</p>
+                        <span><span style={{ color: '#777' }}>Elon:</span> is awazing. I am</span>
+                    </div>
+                </div>
+                <div className={Class.message}>
+                    <img src="https://miro.medium.com/max/2400/1*hgVVYMtzC2FjK221f-6wyA.png" alt="" />
+                    <div className={Class.messageInfo}>
+                        <p>Igor Link</p>
+                        <span><span style={{ color: '#777' }}>Elon:</span> is awazing. I am</span>
+                    </div>
+                </div>
+                <div className={Class.message}>
+                    <img src="https://miro.medium.com/max/2400/1*hgVVYMtzC2FjK221f-6wyA.png" alt="" />
+                    <div className={Class.messageInfo}>
+                        <p>Igor Link</p>
+                        <span><span style={{ color: '#777' }}>Elon:</span> is awazing. I am</span>
+                    </div>
+                </div>
+                <div className={Class.message}>
+                    <img src="https://miro.medium.com/max/2400/1*hgVVYMtzC2FjK221f-6wyA.png" alt="" />
+                    <div className={Class.messageInfo}>
+                        <p>Igor Link</p>
+                        <span><span style={{ color: '#777' }}>Elon:</span> is awazing. I am</span>
+                    </div>
+                </div>
+                <div className={Class.message}>
+                    <img src="https://miro.medium.com/max/2400/1*hgVVYMtzC2FjK221f-6wyA.png" alt="" />
+                    <div className={Class.messageInfo}>
+                        <p>Igor Link</p>
+                        <span><span style={{ color: '#777' }}>Elon:</span> is awazing. I am</span>
+                    </div>
+                </div>
+                <div className={Class.message}>
+                    <img src="https://miro.medium.com/max/2400/1*hgVVYMtzC2FjK221f-6wyA.png" alt="" />
+                    <div className={Class.messageInfo}>
+                        <p>Igor Link</p>
+                        <span><span style={{ color: '#777' }}>Elon:</span> is awazing. I am</span>
+                    </div>
+                </div>
+            </div>
+            <div className={Class.chat}>
+                <i class="fa fa-commenting-o" aria-hidden="true"></i>
+                <p>Choose the user</p>
+            </div>
+        </div>
     )
 }
 
